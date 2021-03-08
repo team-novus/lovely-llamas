990a82dab9db65acca826c238bd86768533a9f1c
Orilly <brunocollingridge@gmail.com>
2021-03-08 20:59:41 +0000

    LovelyLlamas.java + Main Package

---
+++ src/main/java/com/brunocollingridge/lovelyllamas/LovelyLlamas.java
@@ -0,0 +1,25 @@
+package com.brunocollingridge.lovelyllamas;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("lovely_llamas")
+public class LovelyLlamas
+{
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public LovelyLlamas() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+}
