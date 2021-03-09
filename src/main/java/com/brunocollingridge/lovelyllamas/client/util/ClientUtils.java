128376ed51cf67fc849e26d2c8fd8c46d9226ffc
Orilly <brunocollingridge@gmail.com>
2021-03-09 14:41:24 +0000

    ClientUtils

---
+++ src/main/java/com/brunocollingridge/lovelyllamas/client/util/ClientUtils.java
@@ -0,0 +1,9 @@
+package com.brunocollingridge.lovelyllamas.client.util;
+
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class ClientUtils {
+    
+}
