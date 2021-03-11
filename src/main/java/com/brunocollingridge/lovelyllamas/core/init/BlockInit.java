ad2a1f449b1f00f9ea2cb9785c4bce402130fd67
Orilly <brunocollingridge@gmail.com>
2021-03-11 08:41:03 +0000

    Update BlockInit.java

--- src/main/java/com/brunocollingridge/lovelyllamas/core/init/BlockInit.java
+++ src/main/java/com/brunocollingridge/lovelyllamas/core/init/BlockInit.java
@@ -1,12 +1,22 @@
 package com.brunocollingridge.lovelyllamas.core.init;
 
 import com.brunocollingridge.lovelyllamas.LovelyLlamas;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.material.MaterialColor;
+import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraftforge.common.ToolType;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class BlockInit {
+
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS,
             LovelyLlamas.Mod_ID);
+
 }
